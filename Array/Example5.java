21b229210e081f1fd7c61485f05b8a24c077166f
KandukuruBhagyasree <163431750+KandukuruBhagyasree@users.noreply.github.com>
2025-03-24 15:02:25 +0530

    20/03/2025

---
+++ Array/Example5.java
@@ -0,0 +1,20 @@
+public class Example5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[]= {0,1,1,1,0,1,0};
+		int n=arr.length;
+		
+		Arrays.sort(arr);
+		
+				for(int i=0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		
+		
+		
+	}
+
+}
