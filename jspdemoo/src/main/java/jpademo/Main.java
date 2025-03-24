9c8becb84a6dde7aae3bc034057b29117b18bc18
KandukuruBhagyasree <163431750+KandukuruBhagyasree@users.noreply.github.com>
2025-03-24 15:05:27 +0530

    21/03/2025

---
+++ jspdemoo/src/main/java/jpademo/Main.java
@@ -0,0 +1,43 @@
+package jpademo;
+ 
+import jakarta.persistence.EntityManager;
+ 
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		
+		EntityManager em=JPAUtil.getEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Employee emp1=new Employee("prem_vishnu","Training",500);
+		
+		em.persist(emp1);
+		
+		
+		
+		 Employee emp2=em.find(Employee.class,1);
+		
+		 System.out.println(emp2.getName());
+		 System.out.println(emp2.getId());
+		
+		 emp2.setSalary(5000);
+		
+		 em.merge(emp2);
+		
+		 em.remove(emp2);
+		
+		 em.getTransaction().commit();
+		
+		
+		em.close();
+		
+		JPAUtil.close();
+		
+		
+		
+		
+	}
+ 
+}
